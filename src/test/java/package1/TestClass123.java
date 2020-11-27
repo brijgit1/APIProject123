a5618612d871ab7bc85a68df29a34035a163e014
Brij Bhan Singh <Singh_Br@India.XoriantCorp.com>
2020-11-27 16:37:05 +0530

    Test Commit

---
+++ src/test/java/package1/TestClass123.java
@@ -0,0 +1,13 @@
+package package1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass123 {
+	
+	@Test
+	public void testMethod() {
+		Assert.assertTrue(true);
+	}
+
+}
